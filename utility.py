8189b80b5ba83c8bc60eba833f23445921167ff6
paulhjwu <paulhjwu@gmail.com>
2022-09-14 09:54:21 +0800

    Trial #2

--- utility.py
+++ utility.py
@@ -5,11 +5,11 @@ def printPattern(symbol, row=1, col=1):
         print( symbol*col )
 
 
-if __name__ == '__main__':
-    printPattern('*', 2, 3)
-    printPattern('#') #default is row 1, col 1
-    printPattern('$', 3) # row =3 , default col = 1
-    printPattern('%', col=2) #use default row=1, specify col = 2
-    printPattern(symbol='&', row=1, col=2)
+# if __name__ == '__main__':
+#     printPattern('*', 2, 3)
+#     printPattern('#') #default is row 1, col 1
+#     printPattern('$', 3) # row =3 , default col = 1
+#     printPattern('%', col=2) #use default row=1, specify col = 2
+#     printPattern(symbol='&', row=1, col=2)
 
 print( __name__ ) #__main__
